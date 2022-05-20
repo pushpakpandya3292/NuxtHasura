aee1ec93ec1c6a451f94bd4e68e4d4c42c9a2af4
pushpakpandya3292 <pushpakpandya3292@gmail.com>
2022-05-20 12:59:56 +0530

    Initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,113 @@
 <template>
-  <div class="container">
-    <div class="rounded border m-2">
+  <div class="container m-auto">
+       <div class="rounded border m-2">
       <h1 class="p-2 text-4xl font-semi-bold text-gray-800">
         Nuxt.js Querying a GraphQL API
       </h1>
-      <div class="flex p-2" v-if="!$apollo.queries.characters.loading">
-        <ul class="w-100 px-2 rounded border text-gray-600">
-          <li
-            v-for="character in characters.results"
-            :key="character.id"
-            class="py-2"
-          >
-            <nuxt-link
-              :to="character.id"
-              class="w-full inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-black uppercase transition bg-gray-100 rounded shadow ripple hover:shadow-lg hover:bg-gray-200 focus:outline-none"
-            >
-              {{ character.name }}:
-            </nuxt-link>
-            <div
-                v-if="!$apollo.queries.characters.loading"
-                class="rounded border bg-gray-900 ml-2 flex"
-              >
-                <img :src="character.image" :alt="character.name" class="p-2 rounded" />
-                <div class="p-2">
-                  <span class="text-white">{{ character.status }}</span>
-                  <h1 class="text-white text-2xl font-semibold">{{ character.name }}</h1>
-                  <h1 class="text-white">{{ character.gender }}</h1>
-                  <h1 class="text-white">{{ character.origin.name }}</h1>
-                </div>
-              </div>
-          </li>
-          
-        </ul>
-        <div class="flex-grow min-h-full">
-          <nuxt-child :key="$route.params.id"></nuxt-child>
-        </div>
-      </div>
     </div>
+     <div class="container mx-auto">
+            <div class="flex flex-col">
+                <div class="w-full">
+                    <div class="p-4 border-b border-gray-200 shadow">
+                        <!-- <table> -->
+                        <table id="dataTable" class="p-4 w-full">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th class="p-8 text-xs text-gray-500">
+                                        ID
+                                    </th>
+                                    <th class="p-8 text-xs text-gray-500">
+                                        Name
+                                    </th>
+                                    <th class="p-8 text-xs text-gray-500">
+                                        Email
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white">
+                                <tr class="whitespace-nowrap" v-for="data in hazura_hajura_table?hazura_hajura_table.slice((page - 1) * 10, page * 10):[]" :key="data.id">
+                                    <td class="px-6 py-4 text-sm text-center text-gray-500">
+                                      {{data.id}}
+                                    </td>
+                                    <td class="px-6 py-4 text-center">
+                                        <div class="text-sm text-gray-900">
+                                            {{data.name}}
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 text-center">
+                                        <div class="text-sm text-gray-500">{{data.email}}</div>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                         <Pagination
+                            :page="page"
+                            :totalPages="
+                              hazura_hajura_table
+                                ? Array(Math.ceil(hazura_hajura_table.length / 10))
+                                    .fill(0)
+                                    .map((e, i) => i + 1)
+                                : []
+                            "
+                            :name="'users'"
+                            :count="hazura_hajura_table ? hazura_hajura_table.length : 0"
+                            :incrementpage="incrementpage"
+                            :decrementpage="decrementpage"
+                            :setpage="setpage"
+                            :perpage="10"
+                          />
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
-
+import Pagination from '../components/Pagination.vue'
 export default {
+  components:{
+    Pagination
+  },
+  data(){
+    return{
+      page:1
+    }
+  },
+  methods:{
+    incrementpage() {
+      this.page = this.page + 1;
+    },
+    //go to previous oage
+    decrementpage() {
+      this.page = this.page - 1;
+    },
+    //go to perticular page
+    setpage(page) {
+      this.page = page;
+    },
+  },
   apollo: {
-    characters: gql`
-      query getCharacters {
-        characters {
-          results {
-            id
-            name
-            status
-            species
-            gender
-            image
-            origin {
-              name
-            }
-          }
+    hazura_hajura_table: gql`
+      query MyQuery {
+        hazura_hajura_table {
+          id,
+          name,
+          email
         }
       }
     `,
   },
 }
 </script>
+
+<style>
+.main-pagination .pagination {display: flex;justify-content: center;}
+
+.main-pagination .pagination button {width: 35px;height: 35px;border: 2px solid #000;line-height: 30px;margin: 0 10px;}
+
+.main-pagination .pagination button.page-link-active {background: #000;color: #fff;}
+</style>
